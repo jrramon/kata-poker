23ec0272c0ab167afafc1f7347fe240dfba2e296
Joserra <jrramon@gmail.com>
2013-09-30 10:56:39 +0200

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KataPoker::Application.config.secret_token = '794044b2c3574e12221d3502c027ae073c69cdccf84ff7be5a974faeb04f3b7fa5006ee2c28238314f7f848f04e64fcb4c7d7c8099ee0d371c40c4e162bdc580'
