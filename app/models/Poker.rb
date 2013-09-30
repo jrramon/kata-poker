4ac337e6b762dadb4a9db347487dac1c536cbfd9
Joserra <jrramon@gmail.com>
2013-09-30 12:26:44 +0200

    basic case for third

--- app/models/Poker.rb
+++ app/models/Poker.rb
@@ -1,6 +1,32 @@
 class Poker
 
 	def self.check(hand)
-		return :pair
+		return :pair if pair(hand)
+		return :third if third(hand)
+		nil
+	end
+
+	def self.pair(hand) 
+		only_one_pair(counts_appearances(hand))
+	end
+
+	def self.third(hand) 
+		only_one_third(counts_appearances(hand))
+	end
+
+	def self.counts_appearances(hand)
+		counts = {0 => 0}
+		(1..12).each{|n|
+			counts[n] = hand.count{|card| card.num == n } 
+		}
+		counts
+	end
+
+	def self.only_one_pair(counts_apps)
+		counts_apps.values.count(2) == 1 
+	end
+
+	def self.only_one_third(counts_apps)
+		counts_apps.values.count(3) == 1 
 	end
 end
--- spec/kata_poker_spec.rb
+++ spec/kata_poker_spec.rb
@@ -4,13 +4,41 @@ require 'spec_helper'
 describe Poker, "#check" do
 	let (:poker) {Poker.new}
 
+	it "mano no contine :pair" do
+		hand = [ Card.new(1, :diamonds),
+				 Card.new(2, :diamonds),
+				 Card.new(3, :diamonds),
+				 Card.new(5, :diamonds),
+				 Card.new(1, :diamonds)]
+		Poker.check(hand).should eq (:pair)
+	end
+
 	it "mano contine :pair" do
 		hand = [ Card.new(1, :diamonds),
 				 Card.new(2, :diamonds),
 				 Card.new(3, :diamonds),
 				 Card.new(3, :diamonds),
 				 Card.new(1, :diamonds)]
-		Poker.check(hand).should eq (:pair)
+		Poker.check(hand).should_not eq (:pair)
+	end
+
+
+	it "mano no contiene nada" do
+		hand = [ Card.new(1, :diamonds),
+				 Card.new(10, :diamonds),
+				 Card.new(3, :pikes),
+				 Card.new(6, :diamonds),
+				 Card.new(5, :hearts)]
+		Poker.check(hand).should eq (nil)
+	end
+
+	it "mano contiene :third" do
+		hand = [ Card.new(1, :diamonds),
+				 Card.new(3, :diamonds),
+				 Card.new(3, :pikes),
+				 Card.new(6, :diamonds),
+				 Card.new(3, :hearts)]
+		Poker.check(hand).should eq (:third)
 	end
 
 end
